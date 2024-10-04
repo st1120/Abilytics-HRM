db39908a4550d3d067268f462a25f17439a4bbc3
st1120 <sahiltitto1120@gmail.com>
2024-10-04 12:55:58 +0530

    done

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ echo "Waiting for database to be ready..."
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-python3 manage.py createhorillauser --first_name Abilytics --last_name HRM --username abilytics --password 123 --email hr@abilytics.com --phone 8826659270
+python3 manage.py createhorillauser --first_name Abilytics --last_name Inc --username abilytics --password 123 --email hrm@abilytics.com --phone 1234567890
 gunicorn --bind 0.0.0.0:8000 horilla.wsgi:application
